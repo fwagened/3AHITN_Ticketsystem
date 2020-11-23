e4af3b9b76cb2a5a668e30290b0a434682803f22
Stefan <sspahr@htl-steyr.ac.at>
2020-11-23 15:13:02 +0100

    Stefans Änderungen alles neu

--- src/applications/controller/PriorityController.java
+++ src/applications/controller/PriorityController.java
@@ -1,11 +1,18 @@
 package applications.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class PriorityController {
+    public TextField prioritiTextField;
+    public ListView listViewPriorities;
+
     public void saveButtonClicked(ActionEvent actionEvent) {
+        
     }
 
     public void loeschenButtonCLicked(ActionEvent actionEvent) {
+
     }
 }
